4ac778a7823fb14d59bffdc91eade69fed30b972
DESKTOP-UOKCVG7\Paul  Yamg <17py10@queensu.ca>
2020-11-10 12:31:59 -0500

    add Class/Class.cpp

---
+++ Class/Class.cpp
@@ -0,0 +1,39 @@
+
+class Course {
+	// declare attributes, string name, description, credits
+	// float CEAB
+
+	// string course description pulled from txt file?
+	// prerequisites pulled from txt file?
+
+	// place overloaded constructors inside here to create classes
+
+	void get_Courses{
+
+		// accessors, retrieve private variable and set them
+
+	}
+
+	void set_Courses{
+
+		// mutators, sets attributes in course
+
+	}
+
+
+	boolean PrerequisiteCheck{
+
+	case switch / if statements - > check if prerequisites are fulfilled for this course
+
+	}
+
+		int exceptionCheck{
+
+		if staements - > check for exceptions where you can't take two courses together
+
+	}
+
+		int timetable ? ? ? ? check for time table or nah
+
+			if Course == Course
+				// throw CourseException("You've already selected to this course")
