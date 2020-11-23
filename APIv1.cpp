d97b069ab4ad1263cf587e734f217c3a706d7185
Kyle McLellan <17kjm6@queensu.ca>
2020-11-23 14:56:14 -0500

    added cpr library for testing functionality. Updated API key

--- APIv1.cpp
+++ APIv1.cpp
@@ -5,7 +5,7 @@
 
 //https://api.qmulus.io/v1/courses
 std::string api(std::string coursecode) {
-	cpr::Response r = cpr::Get(cpr::Url{ "https://api.qmulus.io/v1/courses/" + coursecode + "?token=b6da7d801b0911ebb55bed7258180e9f" });
+	cpr::Response r = cpr::Get(cpr::Url{ "https://api.qmulus.io/v1/courses/" + coursecode + "?token=eb507c002ce711ebbab92dcdc6160728" });
 
 	//std::cout << r.text << std::endl;
 	return r.text;
@@ -13,10 +13,6 @@ std::string api(std::string coursecode) {
 int main() {
 	std::string info = api("CMPE-320");
 	std::cout << info << std::endl;
-	char units = info[info.length() - 1];
-	std::cout << units << std::endl;
-	for (int i = 0; i < info.length(); i++) {
-		std::cout << info[i] << std::endl;
 	}
 	return 0;
 }
